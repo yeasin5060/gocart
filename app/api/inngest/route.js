8a0c5d0f7a1ee021904f982745292f9ab814fb40
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-09 16:18:33 +0600

    inngest route problem solve

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,4 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
 import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/function";
 
 
