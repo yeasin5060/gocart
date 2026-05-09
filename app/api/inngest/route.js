1b8a26c1b7130164c00bf6d081c60e3467cf543d
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-09 15:43:00 +0600

    all function import inngest route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
+import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+  ],
 });
