489b956d39df0293b1d1af9188890c2431182138
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-09 15:33:04 +0600

    cpmplete the inngest userupdation function

--- inngest/function.js
+++ inngest/function.js
@@ -19,3 +19,24 @@ export const syncUserCreation = inngest.createFunction(
         })
   }
 );
+
+// ✅ inngest function to the Update user
+
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-update",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const {data} = event;
+
+    await prisma.user.update({
+        where : {id : data.id},
+        data : {
+            email:data?.email_addresses[0].email_address,
+            name:data?.first_name + " " +data?.last_name,
+            imageUrl:data?.image_url,
+        }
+    })
+  }
+);
