c79d53bb51011b002a7ba58d896ccce0a54d8aa3
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-10 12:14:36 +0600

    complete the store create route

--- app/api/store/create/route.js
+++ app/api/store/create/route.js
@@ -1,3 +1,4 @@
+import imagekit from "@/configs/imagekit";
 import { prisma } from "@/src/db";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
@@ -21,7 +22,7 @@ export async function POST (request) {
         const image = formData.get('image');
 
         if(!name || !username || !description || !email || !contact || !address || !image){
-            return NextResponse.json({message : 'missing store info' , status : 400})
+            return NextResponse.json({message : 'missing store info'},{  status : 400})
         }
 
         // check is user have alredy registered a store
@@ -38,9 +39,46 @@ export async function POST (request) {
         });
 
         if(isUsernameTaken){
-            return NextResponse.json({message : 'username alredy taken' , status : 400})
+            return NextResponse.json({message : 'username alredy taken'},{status : 400})
         }
+        // image upload to imagekit
+        const buffer = Buffer.from(await image.arrayBuffer());
+
+        const response = await imagekit.upload({
+            file : buffer,
+            fileName : image.name,
+            folder : 'logos'
+        });
+        const optimizedImage = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {quality : 'auto'},
+                {format : 'webp'},
+                {with : '512'}
+            ]
+        });
+
+        const newStore = await prisma.store.create({
+            data : {
+                userId,
+                name ,
+                description,
+                username : username.toLowerCase(),
+                email,
+                contact,
+                address,
+                logo : optimizedImage
+            }
+        });
+        //link store to user
+        await prisma.user.update({
+            where : {id : userId},
+            data : {store : {connect : {id : newStore.id}}}
+        });
+
+        return NextResponse.json({message : 'applied, waiting for approval'});
     } catch (error) {
-        
+        console.error(error);
+        return NextResponse.json({message : error.message},{status:400});
     }
 }
