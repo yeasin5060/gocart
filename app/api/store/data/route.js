313de5983826949a881d106dbfd786085ed5ddb3
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-10 19:54:04 +0600

    complete the store data get function

---
+++ app/api/store/data/route.js
@@ -0,0 +1,29 @@
+import { prisma } from "@/src/db";
+import { NextResponse } from "next/server";
+
+//get store info & store products
+export async function GET(request) {
+    try {
+        //get store username from query params
+        const {searchParams} = new URL(request.url);
+        const username = searchParams.get('username').toLowerCase();
+        
+        if(!username){
+            return NextResponse.json({message : 'missing username'},{status:401});
+        }
+        // get store info and instock products with rating
+        const store = await prisma.store.findUnique({
+            where : {userId , isActive : true},
+            include : {product : {include : {rating : true}}}
+        });
+
+        if(!store){
+            return NextResponse.json({message : 'store not found'},{status:401});
+        }
+
+        return NextResponse.json({store});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message : error.message},{status:400});
+    }
+}
