8daebc89f5d7a1d19135bb3cef5ed58942b9f610
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-11 13:19:50 +0600

    complete the admin approve-store route

---
+++ app/api/admin/approve-store/route.js
@@ -0,0 +1,34 @@
+import authAdmin from "@/middlewares/authAdmin";
+import { prisma } from "@/src/db";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+//approve seller
+export async function POST(request) {
+    try {
+        const {userId} = getAuth(request);
+        const isAdmin = await authAdmin(userId);
+
+        if(!isAdmin){
+            return NextResponse.json({message : 'not authorized'},{status:401});
+        }
+        
+        const {storeId , status} = await request.json();
+
+        if(status === 'approve'){
+            await prisma.store.update({
+                where : {id: storeId},
+                data : {status : "approve", isActive: true}
+            });
+        }else if(status === 'rejected'){
+            await prisma.store.update({
+                where : {id: storeId},
+                data : {status : "rejected"}
+            });
+        }
+        return NextResponse.json({message : status + 'successfully'});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message : error.message},{status:400});
+    }
+}
