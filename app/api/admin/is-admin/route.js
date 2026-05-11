a72e2528ca36f9bbc92d7872bd06bf623fc690c8
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-11 13:08:38 +0600

    complete the admin is-admin route

---
+++ app/api/admin/is-admin/route.js
@@ -0,0 +1,19 @@
+import authAdmin from "@/middlewares/authAdmin";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+//auth admin
+export async function GET(request) {
+    try {
+        const {userId} = getAuth(request);
+        const isAdmin = await authAdmin(userId);
+
+        if(!isAdmin){
+            return NextResponse.json({message : 'not authorized'},{status:401});
+        }
+        return NextResponse.json({isAdmin});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message : error.message},{status:400});
+    }
+}
