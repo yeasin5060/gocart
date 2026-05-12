04968fea7e687297fc2d8e2bc9e409bb3c826a6b
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-12 18:55:56 +0600

    fetch the is admin dashboard route in admin page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -2,11 +2,15 @@
 import { dummyAdminDashboardData } from "@/assets/assets"
 import Loading from "@/components/Loading"
 import OrdersAreaChart from "@/components/OrdersAreaChart"
+import { useAuth } from "@clerk/nextjs"
+import axios from "axios"
 import { CircleDollarSignIcon, ShoppingBasketIcon, StoreIcon, TagsIcon } from "lucide-react"
 import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
 
 export default function AdminDashboard() {
 
+    const  {getToken} = useAuth();
     const currency = process.env.NEXT_PUBLIC_CURRENCY_SYMBOL || '$'
 
     const [loading, setLoading] = useState(true)
@@ -26,7 +30,14 @@ export default function AdminDashboard() {
     ]
 
     const fetchDashboardData = async () => {
-        setDashboardData(dummyAdminDashboardData)
+        try {
+            const token = await getToken();
+            const {data} = await axios.get('/api/admin/dashboard',{headers:{Authorization : `Bearer ${token}`}});
+            setDashboardData(data.dashboardData);
+        } catch (error) {
+            console.log(error.message);
+            toast.error(error?.response?.data?.message ||error.message);
+        }
         setLoading(false)
     }
 
--- components/admin/AdminSidebar.jsx
+++ components/admin/AdminSidebar.jsx
@@ -2,26 +2,27 @@
 
 import { usePathname } from "next/navigation"
 import { HomeIcon, ShieldCheckIcon, StoreIcon, TicketPercentIcon } from "lucide-react"
-import Image from "next/image"
 import Link from "next/link"
-import { assets } from "@/assets/assets"
+import { useUser } from "@clerk/nextjs"
+import Image from "next/image"
 
 const AdminSidebar = () => {
 
-    const pathname = usePathname()
-
+    const pathname = usePathname();
+    const {user} = useUser();
     const sidebarLinks = [
         { name: 'Dashboard', href: '/admin', icon: HomeIcon },
         { name: 'Stores', href: '/admin/stores', icon: StoreIcon },
         { name: 'Approve Store', href: '/admin/approve', icon: ShieldCheckIcon },
         { name: 'Coupons', href: '/admin/coupons', icon: TicketPercentIcon  },
     ]
-
+    console.log(user);
+    
     return (
         <div className="inline-flex h-full flex-col gap-5 border-r border-slate-200 sm:min-w-60">
             <div className="flex flex-col gap-3 justify-center items-center pt-8 max-sm:hidden">
-                <Image className="w-14 h-14 rounded-full" src={assets.gs_logo} alt="" width={80} height={80} />
-                <p className="text-slate-700">Hi, GreatStack</p>
+                <Image className="w-14 h-14 rounded-full" src={user.imageUrl} alt="not found" width={80} height={80} />
+                <p className="text-slate-700">{user?.fullName}</p>
             </div>
 
             <div className="max-sm:mt-6">
