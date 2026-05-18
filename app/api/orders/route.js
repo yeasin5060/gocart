1e54e3a341c8e1f77d20b4f80de062e9b9291cc9
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-18 18:57:20 +0600

    complete the order get route

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -113,3 +113,27 @@ export async function POST(request) {
         return NextResponse.json({message : error.message},{status:400});
     }
 }
+
+//get all orders for a user
+export async function GET(request) {
+    try {
+        const {userId } = getAuth(request);
+
+        const orders = await prisma.order.findMany({
+            where : {userId, OR : [
+                {paymentMethod : paymentMethod.COD},
+                {AND : [{paymentMethod : paymentMethod.STRIPE},{isPaid : true}]}
+            ]},
+            include : {
+                orderItems : {include : {product : true}},
+                address : true
+            },
+            orderBy : {createdAt : 'desc'}
+        });
+
+         return NextResponse.json({orders,message : 'order create successfully'});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message : error.message},{status:400});
+    }
+}
