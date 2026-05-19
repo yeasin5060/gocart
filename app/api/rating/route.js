4c4635d82c558dc09711cda5401c1cec266343f6
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-19 11:00:20 +0600

    complete the rating get route

--- app/api/rating/route.js
+++ app/api/rating/route.js
@@ -2,7 +2,7 @@ import { prisma } from "@/src/db";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-
+    //add new rating
 export async function POST(request) {
     try {
         const {userId} = getAuth(request);
@@ -35,6 +35,27 @@ export async function POST(request) {
         });
 
         return NextResponse.json({message:'Rating added successfully', rating : response});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message : error.message},{status:400});
+    }
+}
+
+// get all rating for a user
+export async function GET(request) {
+    try {
+        const {userId} = getAuth(request);
+
+        if(!userId){
+            return NextResponse.json({message : 'Unauthorized'},{status:401});
+        }
+
+        const ratings = await prisma.rating.findMany({
+            where : {userId}
+        });
+
+        return NextResponse.json({ ratings, message : 'All Ratings get successfully'}, {status:200})
+
     } catch (error) {
         console.error(error);
         return NextResponse.json({message : error.message},{status:400});
