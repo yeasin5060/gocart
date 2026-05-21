f02e8f8e901b43903e632b09d789c20373cfb8cf
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-21 09:34:07 +0600

    add stripe payment in the project

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -2,6 +2,7 @@ import { prisma } from "@/src/db";
 import { getAuth } from "@clerk/nextjs/server";
 import { PaymentMethod } from "@prisma/client";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
 
 export async function POST(request) {
@@ -99,6 +100,34 @@ export async function POST(request) {
                 }
             });
             orderIds.push(order.id);
+        }
+
+        if(paymentMethod === 'STRIPE'){
+            const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+            const origin = await request.headers.get('origin');
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types : ['card'],
+                line_items : [{
+                    price_data: {
+                        currency: 'usd',
+                            product_data: {
+                                name: 'Order'
+                            },
+                            unit_amount: Number(Math.round(fullAmount * 100))
+                        },
+                    quantity : 1
+                }],
+                expires_at : Math.floor(Date.now()/1000) + 30 * 60, // current time + 30 minites
+                mode : 'payment',
+                success_url : `${origin}/loading?nextUrl=orders`,
+                cancel_url : `${origin}/cart`,
+                metadata : {
+                    orderIds : orderIds.join(','),
+                    userId,
+                    appId : 'gocart'
+                }
+            });
+            return NextResponse.json({session});
         }
 
         // clear the cart
