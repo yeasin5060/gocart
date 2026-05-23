b2cfbd745b050f0b297ea4335f19eb1fb9215f16
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-23 08:29:15 +0600

    solve the stripe post route problem

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export async function POST(request) {
     try {
         const body = await request.text();
-        const sig = request.get('stripe-signature');
+        const sig = request.headers.get('stripe-signature');
 
         const event = stripe.webhooks.constructEvent(body,sig,process.env.STRIPE_WEBHOOK_SECRET);
 
@@ -72,5 +72,7 @@ export async function POST(request) {
 
 
 export const config = {
-    api : { bodyparser : false}
+    api: {
+        bodyParser: false
+    }
 }
