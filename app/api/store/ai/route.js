69c25bc88d68e1b6d7cf48999c38fa2f104899f4
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-24 22:46:16 +0600

    add the store api route

---
+++ app/api/store/ai/route.js
@@ -0,0 +1,56 @@
+import authSeller from "@/middlewares/authSeller";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+async function main (base64Image, mimeType) {
+    const messages = [
+        {
+            "role": "system",
+            "content": `
+                You are a product listing assistant for an e-commerce store. Your jobs is to analyze an image of a product and generate structured data.
+
+                Respond ONLY with raw JSON (no code block, no markdown, no explanation)
+                The JSON must strictly follow the schema :
+
+                {
+                    "name" : string,             //Short product name
+                    "description" : string,     // Marketing-friendly
+                    description of the product
+                }
+            `
+        },
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": "What is in this image?",
+            },
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": `data:image/jpeg;base64,${base64Image}`
+            },
+            },
+        ],
+        }
+  ];
+}
+
+
+export async function POST (request) {
+    try {
+        const {userId} = getAuth(request);
+        const isSeller = await authSeller(userId);
+
+        if(!isSeller){
+            return NextResponse.json({message : 'not authorized'},{status : 401});
+        }
+
+        const {base64Image,mimeType} = await request.json();
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message : error.message},{status:400});
+    }
+}
